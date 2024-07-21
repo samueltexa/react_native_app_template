d394fdbbb6b727bbfbc8260836aff728bc7fa700
samueltexa <woolardsamuels@gmail.com>
2024-07-21 16:49:23 +0300

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,11 +6,13 @@ import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import StackNavigator from './src/navigation/StackNavigator'
+import DrawerNavigator from './src/navigation/DrawerNavigator'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      <DrawerNavigator></DrawerNavigator>
+      {/* <StackNavigator/> */}
     </NavigationContainer>
     // <View>
     //   <StatusBar backgroundColor={styles.statusBar.backgroundColor} barStyle="light-content" />
