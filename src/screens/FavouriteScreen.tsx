9c7f2fd0ea33e27a100cbcd2cb585c657d77a7b6
samueltexa <woolardsamuels@gmail.com>
2024-07-21 16:51:58 +0300

    add favoriteScreen

---
+++ src/screens/FavouriteScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FavouriteScreen = () => {
+  return (
+    <View>
+      <Text>FavouriteScreen</Text>
+    </View>
+  )
+}
+
+export default FavouriteScreen
+
+const styles = StyleSheet.create({})
