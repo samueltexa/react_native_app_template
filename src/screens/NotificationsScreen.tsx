3ba5235a5a73dc4213cedac70f1228d97d2ed093
samueltexa <woolardsamuels@gmail.com>
2024-07-21 16:50:58 +0300

    add Nofications

---
+++ src/screens/NotificationsScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NotificationsScreen = () => {
+  return (
+    <View>
+      <Text>NotificationsScreen</Text>
+    </View>
+  )
+}
+
+export default NotificationsScreen
+
+const styles = StyleSheet.create({})
